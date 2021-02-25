1df6d2402a9ec63725467e0063156a7fffe67060
Elber Sousa <elbe995@gmail.com>
2021-02-25 15:30:03 -0300

    finalizando

---
+++ semana3/loops/index.js
@@ -0,0 +1,9 @@
+/* Exercício 1 */
+/*o valor impresso seria de 10 pois o valor está recebendo
+o valor de i somado com o mesmo*/
+
+/* Exercício 2 */
+/* será impresso no console todos os números da lista que são maiores que 18
+Não, para acessar o indice teria de ser criado uma variavel*/
+////
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
