50c05def79480acf7aee9e694c308fa56e5847e9
Elber-Sousa <elbe995@gmail.com>
2021-03-15 19:21:47 -0300

    finalizado

--- semana6/labenuForm/labenuform/src/App.js
+++ semana6/labenuForm/labenuform/src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Etapa1 from './components/Etapa1';
+import Etapa2 from './components/Etapa2';
+import Etapa3 from './components/Etapa3';
+import Final from './components/Final';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    Etapa: 1
+  };
+
+  renderizaF = () => {
+    switch (this.state.Etapa) {
+      case 1:
+        return <Etapa1 />;
+      case 2:
+        return <Etapa2 />;
+      case 3:
+        return <Etapa3 />;
+      case 4:
+        return <Final />;
+      default:
+        return 'nothing';
+    }
+  };
+
+  prox = () => {
+    this.setState({ Etapa: this.state.Etapa + 1 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.renderizaF()}
+        <br />
+        {this.state.Etapa !== 4 && (
+          <button onClick={this.prox}>Próxima etapa</button>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
